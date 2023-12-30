2512cfb0147b396a308e0e9ab0d4e4588402b0dc
Iftekhar Jahan Soron <67633243+Soron248@users.noreply.github.com>
2023-12-30 19:03:00 +0600

    Timeline updated

--- src/Components/TimeLine/TimeLine.jsx
+++ src/Components/TimeLine/TimeLine.jsx
@@ -6,6 +6,8 @@ import TimelineSeparator from "@mui/lab/TimelineSeparator";
 import TimelineConnector from "@mui/lab/TimelineConnector";
 import TimelineContent from "@mui/lab/TimelineContent";
 import TimelineDot from "@mui/lab/TimelineDot";
+import srn from "@/../public/srn.jpg";
+import Image from "next/image";
 
 const TimeLine = () => {
   return (
@@ -17,13 +19,25 @@ const TimeLine = () => {
             <TimelineConnector />
           </TimelineSeparator>
           <TimelineContent>
-            <div className="bg-white p-2 rounded-md rounded-tl-none shadow-md">
-              <h1 className="font-semibold ">Phase 1</h1>
-              <p className="text-gray-500">
+            <div className="bg-white p-3 rounded-md rounded-tl-none shadow-md">
+              <div className="flex items-center gap-3 mb-3">
+                <Image
+                  src={srn}
+                  alt="logo"
+                  width={200}
+                  height={200}
+                  className="w-10 h-10 rounded-full border-2"
+                />
+                <h1 className="font-semibold ">Phase 1</h1>
+              </div>
+              <p className="text-gray-500 mb-3">
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum
                 eveniet quod dignissimos unde consequatur eaque sapiente eos
                 vero sequi praesentium!
               </p>
+              <span className="py-1 px-2 bg-blue-500 text-white rounded-full">
+                Details
+              </span>
             </div>
           </TimelineContent>
         </TimelineItem>
@@ -34,13 +48,25 @@ const TimeLine = () => {
             <TimelineConnector />
           </TimelineSeparator>
           <TimelineContent>
-            <div className="bg-white p-2 rounded-md rounded-tr-none shadow-md">
-              <h1 className="font-semibold ">Phase 2</h1>
-              <p className="text-gray-500">
+            <div className="bg-white p-3 rounded-md rounded-tr-none shadow-md">
+              <div className="flex items-center justify-end gap-3 mb-3">
+                <h1 className="font-semibold ">Phase 1</h1>
+                <Image
+                  src={srn}
+                  alt="logo"
+                  width={200}
+                  height={200}
+                  className="w-10 h-10 rounded-full border-2"
+                />
+              </div>
+              <p className="text-gray-500 mb-3">
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum
                 eveniet quod dignissimos unde consequatur eaque sapiente eos
                 vero sequi praesentium!
               </p>
+              <span className="py-1 px-2 bg-blue-500 text-white rounded-full">
+                Details
+              </span>
             </div>
           </TimelineContent>
         </TimelineItem>
@@ -50,13 +76,25 @@ const TimeLine = () => {
             <TimelineConnector />
           </TimelineSeparator>
           <TimelineContent>
-            <div className="bg-white p-2 rounded-md rounded-tl-none shadow-md">
-              <h1 className="font-semibold ">Phase 3</h1>
-              <p className="text-gray-500">
+            <div className="bg-white p-3 rounded-md rounded-tl-none shadow-md">
+              <div className="flex items-center gap-3 mb-3">
+                <Image
+                  src={srn}
+                  alt="logo"
+                  width={200}
+                  height={200}
+                  className="w-10 h-10 rounded-full border-2"
+                />
+                <h1 className="font-semibold ">Phase 1</h1>
+              </div>
+              <p className="text-gray-500 mb-3">
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum
                 eveniet quod dignissimos unde consequatur eaque sapiente eos
                 vero sequi praesentium!
               </p>
+              <span className="py-1 px-2 bg-blue-500 text-white rounded-full">
+                Details
+              </span>
             </div>
           </TimelineContent>
         </TimelineItem>
@@ -65,13 +103,25 @@ const TimeLine = () => {
             <TimelineDot variant="outlined" color="success" />
           </TimelineSeparator>
           <TimelineContent>
-            <div className="bg-white p-2 rounded-md rounded-tr-none shadow-md">
-              <h1 className="font-semibold ">Phase 4</h1>
-              <p className="text-gray-500">
+            <div className="bg-white p-3 rounded-md rounded-tr-none shadow-md">
+              <div className="flex items-center justify-end gap-3 mb-3">
+                <h1 className="font-semibold ">Phase 1</h1>
+                <Image
+                  src={srn}
+                  alt="logo"
+                  width={200}
+                  height={200}
+                  className="w-10 h-10 rounded-full border-2"
+                />
+              </div>
+              <p className="text-gray-500 mb-3">
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum
                 eveniet quod dignissimos unde consequatur eaque sapiente eos
                 vero sequi praesentium!
               </p>
+              <span className="py-1 px-2 bg-blue-500 text-white rounded-full">
+                Details
+              </span>
             </div>
           </TimelineContent>
         </TimelineItem>
