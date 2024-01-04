37eb065b735c399e75fdb67c66077cb2708e8c2e
Iftekhar Jahan Soron <67633243+Soron248@users.noreply.github.com>
2024-01-04 17:57:41 +0600

    error solved

--- src/app/reusable/carousel/page.js
+++ src/app/reusable/carousel/page.js
@@ -1,12 +1,21 @@
-import Carousel from "@/Components/Carousel/Carousel";
-import Carousel2 from "@/Components/Carousel/Carousel2";
 import React from "react";
+import dynamic from "next/dynamic";
+
+// Import the Carousel component dynamically to handle client-side rendering
+const DynamicCarousel = dynamic(
+  () => import("@/Components/Carousel/Carousel"),
+  { ssr: false }
+);
+const DynamicNewCarousel = dynamic(
+  () => import("@/Components/Carousel/Carousel2"),
+  { ssr: false }
+);
 
 const page = () => {
   return (
     <section className="max-w-full flex flex-col gap-10 justify-center items-center mx-auto mt-5 py-10 px-5 md:px-20 lg:px-28 ">
-      <Carousel />
-      <Carousel2 />
+      <DynamicCarousel />
+      <DynamicNewCarousel />
     </section>
   );
 };
